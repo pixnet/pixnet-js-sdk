12575c6bc4603a56b5d1a9f34dde668b577d28fe
nobita <nobita@pixnet.tw>
2014-05-06 17:18:47 +0800

    show the data from service

--- test/tests.users.js
+++ test/tests.users.js
@@ -4,6 +4,7 @@ module('pixnet.users', {
 asyncTest("getAccount", function() {
     pixnet.login(function() {
         pixnet.users.getAccount(function(data) {
+            console.log(data);
             equal(0, data.error, data.message);
             start();
         });
@@ -12,7 +13,9 @@ asyncTest("getAccount", function() {
 
 asyncTest("getUser", function() {
     expect(1);
+
     pixnet.users.getUser(function(data) {
+        console.log(data);
         equal(0, data.error, data.message);
         start();
     }, 'admin');
