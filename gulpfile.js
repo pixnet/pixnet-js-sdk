385615cc8f32e2c81b06ffba867ddad1c0248204
nobita <nobita@pixnet.tw>
2014-06-19 18:34:27 +0800

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 var paths = {
-    scripts: ['src/pixnet.coffee', 'src/pixnet.*.coffee']
+    scripts: ['src/pixnet.coffee', 'src/pixnet.*.coffee'],
+    coffee: ['src/*.coffee']
 };
 
 gulp.task('scripts', function() {
@@ -19,7 +20,10 @@ gulp.task('scripts', function() {
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-    gulp.watch(paths.scripts, ['scripts']);
+    gulp.watch(paths.coffee[0], function() {
+        gulp.run('scripts');
+    });
+});
 });
 
 // The default task (called when you run `gulp` from cli)
