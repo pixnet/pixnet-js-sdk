7be0da7e46c5c279c7f9a8f190e64dd8d964335c
nobita <nobita@pixnet.tw>
2014-06-19 19:00:42 +0800

    move function to login callback

--- test/tests.index.js
+++ test/tests.index.js
@@ -2,13 +2,6 @@ module('pixnet.index', {
 });
 
 asyncTest("rate", function() {
-    // anonymous
-    pixnet.index.rate(function(data) {
-        console.log(data);
-        equal(0, data.error, data.message);
-        start();
-    }
-    // Login
     pixnet.login(function() {
         pixnet.index.rate(function(data) {
             console.log(data);
@@ -20,10 +13,11 @@ asyncTest("rate", function() {
 
 asyncTest("now", function() {
     expect(1);
-
-    pixnet.index.now(function(data) {
-        console.log(data);
-        equal(0, data.error, data.message);
-        start();
+    pixnet.login(function() {
+        pixnet.index.now(function(data) {
+            console.log(data);
+            equal(0, data.error, data.message);
+            start();
+        });
     });
 });
