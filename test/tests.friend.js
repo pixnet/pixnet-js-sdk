1f4afa47d8a307d8c58e97772acb21c8fc557211
nobita <nobita@pixnet.tw>
2014-06-23 19:52:51 +0800

    add "group modify" test

--- test/tests.friend.js
+++ test/tests.friend.js
@@ -14,3 +14,25 @@ asyncTest("getGroups", function() {
         });
     });
 });
+
+asyncTest("group modify", function() {
+    expect(3);
+    pixnet.login(function() {
+        pixnet.friend.createGroup(function(data) {
+            console.log(data);
+            equal(0, data.error, data.message);
+            pixapp.friend.groupId = data.friend_group.id;
+
+            pixnet.friend.updateGroup(function(data) {
+                console.log(data);
+                equal(0, data.error, data.message);
+            }, pixapp.friend.groupId, 'update group');
+
+            pixnet.friend.deleteGroup(function(data) {
+                console.log(data);
+                equal(0, data.error, data.message);
+                start();
+            }, pixapp.friend.groupId);
+        }, 'test group');
+    });
+});
