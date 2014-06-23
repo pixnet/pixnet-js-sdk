40739d48fa71e1607aa519b22f89ecccdef51d28
nobita <nobita@pixnet.tw>
2014-06-23 19:54:48 +0800

    add getSubscriptions test

--- test/tests.friend.js
+++ test/tests.friend.js
@@ -86,3 +86,14 @@ asyncTest("getNews", function() {
         start();
     });
 });
+
+asyncTest("getSubscriptions", function() {
+    expect(1);
+    pixnet.login(function() {
+        pixnet.friend.getSubscriptions(function(data) {
+            console.log(data);
+            equal(0, data.error, data.message);
+            start();
+        });
+    });
+});
