3aac72c51c3c4f64d9005d12af4dbdbe269ee97d
nobita <nobita@pixnet.tw>
2014-06-23 18:41:45 +0800

    add setCommentOpen test

--- test/tests.blog.js
+++ test/tests.blog.js
@@ -189,6 +189,17 @@ asyncTest("replyComment", function() {
     });
 });
 
+asyncTest("setCommentOpen", function() {
+    expect(1);
+    pixnet.login(function() {
+        pixnet.blog.setCommentOpen(function(data) {
+            console.log(data);
+            equal(0, data.error, data.message);
+            start();
+        }, pixapp.blog.commentId);
+    });
+});
+
 asyncTest("getSiteCategories", function() {
     expect(1);
     pixnet.login(function() {
