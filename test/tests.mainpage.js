7dbdbba742f435f263adbd6d3936a5c0197ad68e
nobita <nobita@pixnet.tw>
2014-06-23 20:08:24 +0800

    add getBlogColumns test

--- test/tests.mainpage.js
+++ test/tests.mainpage.js
@@ -4,4 +4,13 @@ module('pixnet.mainpage', {
     }
 });
 
+asyncTest("getBlogColumns", function() {
+    expect(1);
+    pixnet.mainpage.getBlogColumns(function(data) {
+        console.log(data);
+        equal(true, pixnet.isArray(data), data);
+        start();
+    });
+});
+
 
