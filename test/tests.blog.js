6527338103b692415c6e442fec7c8d2314fb6f34
nobita <nobita@pixnet.tw>
2014-06-23 18:23:24 +0800

    add getSiteCategories test

--- test/tests.blog.js
+++ test/tests.blog.js
@@ -178,6 +178,13 @@ asyncTest("getSingleComment", function() {
     });
 });
 
+asyncTest("getSiteCategories", function() {
+    expect(1);
+    pixnet.login(function() {
+        pixnet.blog.getSiteCategories(function(data) {
+            console.log(data);
+            equal(0, data.error, data.message);
+            start();
         }, pixapp.blog.userName);
     });
 });
