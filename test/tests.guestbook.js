89503f0d98d79f643369cb3ead6cc91be868f00a
nobita <nobita@pixnet.tw>
2014-06-23 20:01:44 +0800

    add getOne test

--- test/tests.guestbook.js
+++ test/tests.guestbook.js
@@ -15,5 +15,14 @@ asyncTest("getAll", function() {
     }, pixapp.blog.userName);
 });
 
+asyncTest("getOne", function() {
+    expect(1);
+    pixnet.guestbook.getOne(function(data) {
+        console.log(data);
+        equal(0, data.error, data.message);
+        start();
+    }, pixapp.guestbook.artId, pixapp.blog.userName);
+});
+
 
 
