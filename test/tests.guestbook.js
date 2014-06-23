cb4a18c59823ef85184286eae9ac7ee15c9a485e
nobita <nobita@pixnet.tw>
2014-06-23 20:02:30 +0800

    add setOpen test

--- test/tests.guestbook.js
+++ test/tests.guestbook.js
@@ -35,5 +35,16 @@ asyncTest("reply", function() {
     });
 });
 
+asyncTest("setOpen", function() {
+    expect(1);
+    pixnet.login(function() {
+        pixnet.guestbook.setOpen(function(data) {
+            console.log(data);
+            equal(0, data.error, data.message);
+            start();
+        }, pixapp.guestbook.artId);
+    });
+});
+
 
 
