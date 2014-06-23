24d20acdcaba4a57d36a202fdbbeb4bcba1ac4b5
nobita <nobita@pixnet.tw>
2014-06-23 19:34:30 +0800

    add getAlbumMain test

--- test/tests.album.js
+++ test/tests.album.js
@@ -4,3 +4,13 @@ module('pixnet.album', {
     }
 });
 
+asyncTest("getAlbumMain", function() {
+    expect(1);
+    pixnet.login(function() {
+        pixnet.album.getAlbumMain(function(data) {
+            console.log(data);
+            equal(0, data.error, data.message);
+            start();
+        });
+    });
+});
