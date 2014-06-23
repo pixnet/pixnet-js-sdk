10afd9a7baf697e426b2bc2a1a422953b5a09272
nobita <nobita@pixnet.tw>
2014-06-23 19:35:02 +0800

    add getAlbumSetfolders sortSetFolders test

--- test/tests.album.js
+++ test/tests.album.js
@@ -14,3 +14,25 @@ asyncTest("getAlbumMain", function() {
         });
     });
 });
+
+asyncTest("getAlbumSetfolders", function() {
+    expect(2);
+    pixnet.login(function() {
+        pixnet.album.getAlbumSetfolders(function(data) {
+            console.log(data);
+            equal(0, data.error, data.message);
+
+            var ids = [];
+            for (var i = data.setfolders.length; i--;) {
+                ids.push(data.setfolders[i].id);
+            }
+
+            pixnet.album.sortSetFolders(function(data) {
+                console.log(data);
+                equal(true, pixnet.isArray(data.sets), data.message);
+                start();
+            }, ids.toString());
+
+        }, pixapp.blog.userName);
+    });
+});
